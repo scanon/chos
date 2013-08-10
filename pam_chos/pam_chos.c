58bdf4ca6361276f9b55e85e241f4b6837af6f42
Larry Pezzaglia <lmpezzaglia@lbl.gov>
2013-08-09 17:34:46 -0700

    Initial implementation of privilege-dropping helper process

--- pam_chos/pam_chos.c
+++ pam_chos/pam_chos.c
@@ -37,6 +37,7 @@
  * - no chos
  */
 
+#define _GNU_SOURCE
 #define  PAM_SM_SESSION
 #include <security/pam_modules.h>
 
@@ -48,6 +49,8 @@
 #include <errno.h>
 #include <limits.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <grp.h>
 #include <pwd.h>
 #include <fcntl.h>
 
@@ -64,14 +67,13 @@ pam_chos_config *init_pam_chos_config(void) {
 
   pam_chos_config *cfg =
     (pam_chos_config *)malloc(sizeof(pam_chos_config));
-  syslog(LOG_ERR, "cfg ptr: %p\n", cfg);
 
   return memcpy(cfg, &pam_chos_config_default,
     sizeof(pam_chos_config));
 }
 
 int argmatch(const char *arg, const char *match) {
-  if(strlen(arg) < strlen(match)) {
+  if(strlen(arg) < strlen(match)+1) {
     return 0;
   }
   else {
@@ -82,19 +84,18 @@ int argmatch(const char *arg, const char *match) {
 void parse_pam_chos_args(pam_chos_config *args, int argc, const char
         **argv) {
 
-  char **argptr = argv;
 
   while(argc--) {
-    if(argmatch(argptr[0],"user_conf_file=")) {
+    if(argmatch(argv[0],"user_conf_file=")) {
       args->user_conf_file =
-        strndup(argptr[0]+strlen("user_conf_file="),MAX_LEN);
+        strndup(argv[0]+strlen("user_conf_file="),MAX_LEN);
     }
 
-    if(argmatch(argptr[0],"fail_to_default=")) {
+    if(argmatch(argv[0],"fail_to_default=")) {
       args->fail_to_default =
-        atoi(argptr[0]+strlen("fail_to_default="));
+        atoi(argv[0]+strlen("fail_to_default="));
     }
-    argptr++;
+    argv++;
   }
 }
 
@@ -104,16 +105,19 @@ PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags,
   int ret = PAM_SESSION_ERR;
   int onerr = PAM_SUCCESS;
   char const *user;
+  FILE *f;
   struct passwd *pw;
+  int gid;
   char *os;
+  char env_path[MAXLINE+1];
   const char *env;
   char osenv[MAXLINE+1];
   char envvar[50];
   int usedefault=0;
+  int child_pid, child_status;
+  int fd[2];
   pam_chos_config *cfg;
-  unsigned int oldeuid;
 
-  oldeuid = geteuid();
   
   openlog("pam_chos", LOG_PID, LOG_AUTHPRIV);
   
@@ -123,6 +127,7 @@ PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags,
   }
   
   pw=getpwnam(user);
+  gid = getgrnam(user)->gr_gid;
   if (pw->pw_uid==0){
     return PAM_SUCCESS;
   }
@@ -131,54 +136,156 @@ PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags,
     return ret;
   }
 
-  cfg = init_pam_chos_config();
-  parse_pam_chos_args(cfg, argc, argv);
+  pipe(fd);
+  child_pid = fork();
 
-
-  seteuid(pw->pw_uid);
-
-  if ((env=getenv("CHOS"))){
-    strncpy(osenv,env,MAXLINE);
+  /* Fork failed */
+  if (child_pid == -1) {
+    syslog(LOG_ERR,"failed to fork(): %s\n",strerror(errno));
+    close(fd[0]);
+    close(fd[1]);
+    return child_pid;
   }
-  else{
+  else if (child_pid == 0) {
+    /* Child process */
+
+    close(fd[0]);
+    //dup2(fd[1], STDOUT_FILENO);
+    //close(fd[1]);
+
+
+    /* Drop privileges */
+    if(setresgid(gid, gid, gid) != 0 ) {
+        syslog(LOG_ERR, "setresgid failed: %s", strerror(errno));
+        close(fd[1]);
+        exit(-1);
+    }
+
+    if(setresuid(pw->pw_uid, pw->pw_uid, pw->pw_uid) != 0 ) {
+        syslog(LOG_ERR, "setresuid failed: %s", strerror(errno));
+        close(fd[1]);
+        exit(-1);
+    }
+
+    syslog(LOG_ERR,"uid, gid, euid, egid: %d, %d, %d, %d\n",
+            getuid(), getgid(), geteuid(), getegid());
+
+    cfg = init_pam_chos_config();
+    parse_pam_chos_args(cfg, argc, argv);
+
     read_chos_file(cfg->user_conf_file, pw->pw_dir, osenv);
-    if (osenv[0]==0){
-      syslog(LOG_ERR,"CHOS not set\n");
-      return onerr;
-    }
-  }
-  os=check_chos(osenv);
 
-  if (os == NULL) {
-    if (strcmp(osenv,DEFAULT_ENV_NAME)==0){
-      usedefault=1;  
+    if ((env=getenv("CHOS"))){
+      strncpy(osenv,env,MAXLINE);
     }
-    else if (cfg->fail_to_default) {
-      /* Fail back to the default CHOS */
-      syslog(LOG_WARNING,
-        "Warning: requested os (%s) is not recognized; using default\n",osenv);
-      os=check_chos(strdup(DEFAULT_ENV_NAME));
+    else{
+      if (osenv[0]==0){
+        syslog(LOG_ERR,"CHOS not set\n");
+        close(fd[1]);
+        exit(onerr);
+      }
     }
-    else {
-      /* Try the (likely invalid) environment name. */
-      syslog(LOG_WARNING,
-              "Warning: requested os (%s) is not recognized\n",osenv);
-      os=osenv;
+    os=check_chos(osenv);
+
+    free(cfg->user_conf_file);
+    free(cfg);
+
+    if (os == NULL) {
+      if ( (strcmp(osenv,DEFAULT_ENV_NAME)==0) || 
+         (cfg->fail_to_default > 0) ) {
+        /* Fail back to the default CHOS */
+        syslog(LOG_WARNING,
+          "Warning: requested os (%s) is not recognized; using default (fail_to_default=%d).\n",
+          osenv, cfg->fail_to_default);
+        os=check_chos(strdupa(DEFAULT_ENV_NAME));
+      }
+      else {
+        /* Try the (likely invalid) environment name. */
+        syslog(LOG_WARNING,
+                "Warning: requested os (%s) is not recognized.\n",osenv);
+        os=osenv;
+      }
     }
+
+    f = fdopen(fd[1],"w");
+    if(!f) {
+      syslog(LOG_ERR, "fdopen() failed: %s\n",strerror(errno));
+      exit(-1);
+    }
+    syslog(LOG_INFO,"Environment name: %s path: %s \n",osenv, os);
+    fputs(os,f);
+    fputc('\n',f);
+    fputs(osenv,f);
+    fputc('\n',f);
+
+    fclose(f);
+    exit(0);
   }
+  /* Parent process */
+  else {
+
+    close(fd[1]);
+    syslog(LOG_INFO,"forked pid %d\n",child_pid);
+    syslog(LOG_ERR,"uid, gid, euid, egid: %d, %d, %d, %d\n",
+            getuid(), getgid(), geteuid(), getegid());
+
+    f = fdopen(fd[0],"r");
+
+    if(!f) {
+      syslog(LOG_ERR, "fdopen() failed: %s\n",strerror(errno));
+      return(onerr);
+    }
+
+
+    if(!(read_line_from_fd(f, env_path))) {
+      syslog(LOG_ERR, "Failed to read path from child: %s\n",strerror(errno));
+      fclose(f);
+      wait(&child_status);
+      return(onerr);
+    }
+    
+    if(!(read_line_from_fd(f, osenv))) {
+      syslog(LOG_ERR, "Failed to read environment name from child: %s\n",
+              strerror(errno));
+      fclose(f);
+      wait(&child_status);
+      return(onerr);
+    }
+
+    fclose(f);
+    wait(&child_status);
+
+    if(child_status != 0) {
+      syslog(LOG_ERR, "Child returned status: %s\n",child_status);
+      return(onerr);
+    }
+
+    sanitize_path(env_path, MAXLINE);
+    sanitize_name(osenv, MAXLINE);
+    syslog(LOG_INFO, "Activate child environment: %s at %s\n",osenv,env_path);
+
   
-  if (usedefault==0 && set_multi(os)!=0){
-    syslog(LOG_ERR,"Failed to set OS to requested system.\n");
+    if (usedefault==0 && set_multi(env_path)!=0){
+      syslog(LOG_ERR,"Failed to set OS to requested system.\n");
+      return ret;
+    }
+
+
+    snprintf(envvar,45,"CHOS=%.40s",osenv);
+    pam_putenv(pamh, envvar);
+    closelog();
+    ret = PAM_SUCCESS;
     return ret;
   }
-
-  sprintf(envvar,"CHOS=%.40s",osenv);
-  pam_putenv(pamh, envvar);
-  seteuid(oldeuid);
-  closelog();
-  ret = PAM_SUCCESS;
-  return ret;
 }
+
+/* Read a line of up to MAXLINE-1 from an open FD */
+/* Sufficient storage must be available at *dest */
+char *read_line_from_fd(FILE *f, char *dest) {
+  return fgets(dest, MAXLINE, f);
+}
+
+
 
 PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags,
             int argc, const char **argv)
@@ -290,25 +397,73 @@ int read_chos_file(char *user_conf_file, char *dir, char *osenv)
   int count=0;
   char userfile[MAX_LEN+1];
 
-  sprintf(userfile,"%.100s/%.10s",dir,user_conf_file);
+  snprintf(userfile,MAX_LEN,"%.100s/%.20s",dir,user_conf_file);
   conf=open(userfile,O_RDONLY);
   if (conf>=0){
     count=read(conf,osenv,MAXLINE-1);
     osenv[count]=0;
     close(conf);
   }
+  else {
+    syslog(LOG_WARNING,
+      "Warning: Could not open user configuration file (%s): %s\n",
+      userfile, strerror(errno));
+  }
   if (count==0) {
-    char *default_env_name = strndup(DEFAULT_ENV_NAME,MAXLINE-1);
-    count = strlen(default_env_name);
-    strncpy(osenv, default_env_name, count+1);
-    free(default_env_name);
+    snprintf(osenv, MAXLINE-1, "%s", DEFAULT_ENV_NAME);
+    count=strlen(osenv) + 1;
   }
 
   for (i=0;i<count;i++){
     if ((osenv[i]=='\n') || (osenv[i]=='\0') )
       break;
   }
+  sanitize_name(osenv,count);
 
   osenv[i]=0;
+
+
   return count;
+}
+
+void sanitize_name(char *s, int length) {
+  sanitize_str(s, length, 0);
+  return;
+}
+
+void sanitize_path(char *s, int length) {
+  sanitize_str(s, length, 1);
+  return;
+}
+
+void sanitize_str(char *s, int length, int is_path) {
+  int i;
+  for (i = 0; i < length; i++) {
+    if (s[i] == '\n') {
+        s[i] = '\0';
+    }
+    if (!(is_valid_char(s[i], is_path))) {
+      s[i]='_';
+    }
+  }
+  return;
+}
+
+int is_valid_char(char c, int is_path) {
+  int is_valid = 0;
+
+  /* Allow [0-9a-zA-Z-._] and '/' if is_path is true */
+  if  (
+        (c == '\0') ||
+        (c == '-') ||
+        (c == '.') ||
+        ((c >= '0') && (c <= '9')) ||
+        ((c >= '/') && (is_path)) ||
+        ((c >= 'a') && (c <= 'z')) ||
+        (c == '_') ||
+        ((c >= 'A') && (c <= 'Z')) 
+      ) is_valid = 1;
+
+  /* syslog(LOG_ERR,"char %c,%d read %d\n",c,c,is_valid); */
+  return is_valid;
 }
--- pam_chos/pam_chos.h
+++ pam_chos/pam_chos.h
@@ -33,3 +33,10 @@ void parse_pam_chos_args(pam_chos_config *args, int argc, const char
   **argv);
 
 int argmatch(const char *arg, const char *match);
+
+int is_valid_char(char c, int is_path);
+
+void sanitize_name(char *s, int length);
+void sanitize_path(char *s, int length);
+void sanitize_str(char *s, int length, int is_path);
+char *read_line_from_fd(FILE *f, char *dest);
